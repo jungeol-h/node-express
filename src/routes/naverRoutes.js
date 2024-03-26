0b87078165d8d5d16ca5dcf9b27f338208dca1ef
jungeol <dali2728@gmail.com>
2024-03-26 14:28:19 +0900

    Add new route to fetch product order details

--- src/routes/naverRoutes.js
+++ src/routes/naverRoutes.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { handleError } = require("../utils/error");
 const naverService = require("../services/naverService"); // naverService를 import 합니다.
+const {
+  fetchAuthToken,
+  fetchProductOrderDetails,
+} = require("../services/naverService");
 
 router.get("/test-fetch", async (req, res) => {
   try {
@@ -12,6 +16,22 @@ router.get("/test-fetch", async (req, res) => {
     await naverService.insertProductOrderDetails(lastChangedFromInput); // naverService로 로직 이동
 
     res.send("업데이트 테스트!");
+  } catch (error) {
+    handleError(res, error);
+  }
+});
+
+router.get("/get-product-order-details", async (req, res) => {
+  try {
+    const authToken = await fetchAuthToken();
+
+    const ids = req.query.ids;
+    if (!ids) {
+      return res.status(400).send("상품 ID를 입력해주세요.");
+    }
+
+    const details = await fetchProductOrderDetails(authToken.access_token, ids);
+    res.json(details);
   } catch (error) {
     handleError(res, error);
   }
